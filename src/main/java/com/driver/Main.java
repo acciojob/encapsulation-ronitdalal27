13a4ecbc6fe3aae0fb9f039f8c4fb9a6c3ab67eb
ronitdalal27 <dalalronit131@gmail.com>
2024-05-12 21:48:02 +0530

    solution1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+    RWOnly obj = new RWOnly();
+    //obj.name = "ronit";
+    //System.out.println(obj.name);
+    //you cannot access private data members of any class and can't print it 
+    obj.setter("Ronit");
+    System.out.println(obj.getter());
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public void setter(String name){
+        this.name = name;
+    }
+
+    public String getter(){
+        return this.name;
+    }
 }
